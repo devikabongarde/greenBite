54f0e3b560088a5bbb128dfe59d57828dcb011e2
Devika Bongarde <devikabongarde@gmail.com>
2025-03-03 21:18:45 +0530

    logo

--- src/components/ui/app-sidebar.jsx
+++ src/components/ui/app-sidebar.jsx
@@ -105,7 +105,7 @@ export function AppSidebar(props) {
             <SidebarMenuButton size="lg" asChild>
               <a href="/">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Command className="size-4" />
+                  <img src="/icon.png" alt="Logo" className="size-6" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">GreenBite</span>
